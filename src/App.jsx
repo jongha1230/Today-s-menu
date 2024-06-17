f52668f27dc768cce2ee63a54508904889cf6977
chasyuss <cotn963@gmail.com>
2024-06-17 20:51:19 +0900

    🎉 add: first setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
-import "./App.css";
 import router from "./routers/router";
+import { Homepage } from "./components/pages/MainPage/Homepage";
 
 function App() {
   return <RouterProvider router={router} />;
--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,5 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Homepage } from "../components/pages/MainPage/Homepage";
+import { MainLayout } from "../styles/MainLayout";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Homepage />,
+            }
+        ]
+    }
+]);
 
 export default router;
+
+
