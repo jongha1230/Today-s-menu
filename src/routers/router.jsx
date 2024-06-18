04148d0f8b609e400305f309cec20dc089b5e333
YIS <dtdt203@naver.com>
2024-06-18 10:11:45 +0900

    add : RecipeDetail 페이지 생성
    - UI 90%

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,5 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
+import RecipeDetail from '../pages/RecipeDetail';
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    // path: '/recipe-detail',
+    path: '/',
+    element: <RecipeDetail />
+  }
+]);
 
 export default router;
