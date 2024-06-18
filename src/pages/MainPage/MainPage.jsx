5dfc2f2a546a88ce7994369da028119bdc36b668
chasyuss <cotn963@gmail.com>
2024-06-18 12:44:56 +0900

    ✨ feat: 슈퍼베이스 db 연결

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MainImage from '../../assets/images/MainImage.jpg'
+import supabase from '../../api/supabaseAPI';
+
 
 
 const MainPage = () => {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        const Cards = async () => {
+            const { data, error } = await supabase.from('recipes').select('*');
+            if (error) {
+                console.error('데이터 에러', error);
+            } else {
+                setCards(data);
+            }
+        };
+        Cards();
+    }, []);
+
     return (
         <div>
             <div alt="사진박스" className="bg-gray-200 w-300 h-48 flex items-center justify-center mt-5">
@@ -12,27 +28,27 @@ const MainPage = () => {
             <div alt="설문조사" className="bg-gray-200 w-300 h-32 flex items-center justify-center mt-5"> 설문조사</div>
 
             <div className="flex flex-wrap justify-center space-x-4 mt-5">
-                <div className="bg-white w-52 h-48 rounded-lg p-4 border-2">
+                {cards.map((card) => (
+                    <div key={card.id} className="bg-white w-52 h-48 rounded-lg p-4 border-2">
+                        <image> {card.thumbnail} </image>
+                        <div className='p-4'>
+                            <hr className="border-gray-300 mb-2" />
+                            <h3 className="text-lg font-bold mb-2"> {card.title} </h3>
+                            <p className="text-gray-600"> {card.nickname} </p>
+                            <p className='text-gray-600'> {new Date(card.created_at).toLocaleDateString()} </p>
+                        </div>
+                    </div>
+                ))}
+                {/* {/* <div className="bg-white w-52 h-48 rounded-lg p-4 border-2">
                     <h3 className="text-lg font-bold mb-2"> 음식 1 </h3>
                     <p className="text-gray-600"> 사용자1 </p>
-                </div>
-                <div className="bg-white w-52 h-48 rounded-lg p-4 border-2">
-                    <h3 className="text-lg font-bold mb-2"> 음식 2</h3>
-                    <p className="text-gray-600"> 사용자2 </p>
-                </div>
-                <div className="bg-white w-52 h-48 rounded-lg p-4 border-2">
-                    <h3 className="text-lg font-bold mb-2"> 음식 3</h3>
-                    <p className="text-gray-600"> 사용자3 </p>
-                </div>
-                <div className="bg-white w-52 h-48 rounded-lg p-4 border-2">
-                    <h3 className="text-lg font-bold mb-2"> 음식 4</h3>
-                    <p className="text-gray-600"> 사용자4 </p>
-                </div>
+                </div> */}
 
                 <div>
-                    <a href='#' className='py-4 px-2 rounded-full bg-buttonPink'> 레시피 추가하기 </a>
+                    <a href='#' className='py-4 px-4 rounded-full bg-buttonPink'> 레시피 작성 </a>
                 </div>
             </div>
+
         </div >
     )
 }
