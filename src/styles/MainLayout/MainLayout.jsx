3e44033495065aee2a2f23b61fa6c266275c3acf
Jongha Lim <jongha1230@gmail.com>
2024-06-18 16:26:21 +0900

    🎉 설문조사 모달 구현

--- src/styles/MainLayout/MainLayout.jsx
+++ src/styles/MainLayout/MainLayout.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Header } from '../../components/common/Header/Header';
+import Header from '../../components/common/Header';
 
 const MainLayout = () => {
+const MainLayout = () => {
   return (
+    <>
+      <Header />
     <>
       <Header />
       <Outlet />
       {/* <Footer /> */}
     </>
+      {/* <Footer /> */}
+    </>
   );
+};
 };
 
 export default MainLayout;
