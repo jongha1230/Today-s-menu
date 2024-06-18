6ae148e593f1187b1564d95d089ccb6f98e54dec
chasyuss <cotn963@gmail.com>
2024-06-18 18:59:38 +0900

    ✨ feat: 이미지 등록 확인

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -34,14 +34,14 @@ const MainPage = ({ onSearch }) => {
 
     return (
         <div>
-            <div alt="사진박스" className="bg-gray-200 w-300 h-48 flex items-center justify-center mt-5">
+            <div alt="사진박스" className="bg-gray-200 w-full h-48 flex items-center justify-center mt-5">
                 <img src={MainImage} alt="음식사진" className="object-cover w-full h-full" />
             </div>
 
             <div alt="설문조사" className="bg-gray-200 w-300 h-32 flex items-center justify-center mt-5"> 설문조사</div>
 
             {/* 검색 기능 */}
-            <div className='flex items-center justify-center mt-5'>
+            <div className='flex items-center justify-center mt-2'>
                 <div className='flex items-center bg-white rounded-md px-3 border-2'>
                     <input
                         type='text'
@@ -54,27 +54,32 @@ const MainPage = ({ onSearch }) => {
                         <SearchIcon />
                     </button>
                 </div>
+
+                <div className="flex justify-center mt-5">
+                    <a href='#' className='py-4 px-4 rounded-full  bg-sub-color hover:bg-default-color text-center mb-5 ml-10'>레시피 작성</a>
+                </div>
             </div>
 
             <div className="flex flex-wrap justify-center space-x-4 mt-5">
                 {filteredCards.slice(0, 4).map((card) => (
-                    <div key={card.id} className="bg-white w-52 h-48 rounded-lg p-4 border-2">
-                        <img src={card.image_url} alt={card.title} />
+                    <div key={card.id} className="bg-white w-52 h-74 rounded-lg p-4 border border-2 mb-5">
+                        <div>
+                            <img src={card.thumbnail} alt={card.title} className="object-cover w-full rounded-lg" />
+                        </div>
                         <div className='p-4'>
                             <hr className="border-gray-300 mb-2" />
                             <h3 className="text-lg font-bold mb-2"> {card.title} </h3>
-                            <p className="text-gray-600"> {card.nickname} </p>
                             {/* <p className="text-gray-600"> {card.content} </p> */}
-
+                            {/* <p className="text-gray-600 text-sm mt-2"> {card.nickname} </p>
+                            <p className='text-gray-600 text-sm mt-2'> {new Date(card.created_at).toLocaleDateString()} </p> */}
+                            <p className="text-gray-600 mb-1 text-sm justify-between"> {card.nickname} {new Date(card.created_at).toLocaleDateString()} </p>
                         </div>
-                        <p className='text-gray-600 text-sm mt-2'> {new Date(card.created_at).toLocaleDateString()} </p>
+
                     </div>
                 ))}
+
             </div>
 
-            <div className="flex justify-center mt-5">
-                <a href='#' className='py-4 px-4 rounded-full bg-sub-color hover:bg-default-color  text-center mb-5'>레시피 작성</a>
-            </div>
         </div >
     )
 }
