c6ab84297634fb860855dad8fd6520ca9e3660ae
YIS <dtdt203@naver.com>
2024-06-19 19:23:45 +0900

    엔브수정

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,9 +3,9 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { LuPointer } from 'react-icons/lu';
 import { Link } from 'react-router-dom';
 
-import supabase from '../../api/supabaseAPI';
 import MainImage from '../../assets/images/MainImage.jpg';
 import SurveyModal from '../../components/modals/SurveyModal/SurveyModal';
+import supabase from '../../api/supabaseAPI';
 
 const MainPage = ({ onSearch }) => {
   const [cards, setCards] = useState([]);
