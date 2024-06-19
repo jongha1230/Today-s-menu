d78403e0798483787746be94a8ddada756bcfde7
chasyuss <cotn963@gmail.com>
2024-06-19 11:23:18 +0900

    🔨script: zustand설치

--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
-import { immer } from 'zustand/middleware/immer';
 import { persist } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
 
 const useUserStore = create(
   persist(
