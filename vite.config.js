17a25831986fae37ec0803ce11b4e5e8eff42117
Jongha Lim <jongha1230@gmail.com>
2024-06-24 22:10:16 +0900

    MIME 유형 오류

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: '/src/main.jsx'
+    }
+  },
+  server: {
+    open: true
+  }
 });
