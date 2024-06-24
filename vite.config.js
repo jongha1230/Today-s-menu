ac684839707cc665c31cbe2835b6f723540de712
jongha lim <122885302+jongha1230@users.noreply.github.com>
2024-06-24 22:14:09 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: '/src/main.jsx'
-    }
-  },
-  server: {
-    open: true
-  }
+    outDir: 'dist', 
 });
